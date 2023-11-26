61be1ab94ed2c2af63caa62331994e5afc6537dc
MunyinSam <kelvinsam233@gmail.com>
2023-11-26 15:28:19 +0700

    Upload File

---
+++ Health/Health.cs
@@ -0,0 +1,68 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header ("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("IFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        // Clamp is upper and lower bound
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            //iframes
+            StartCoroutine(Invunerability()); // must call like this 
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability() 
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        // invunerability duration
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration/ (numberOfFlashes * 2));
+            spriteRend.color = Color.white; // can use this instead of creating new color
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
+    
+}
